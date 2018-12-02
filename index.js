ff7c8fc671eadf197544c0f2397d20b10a22f34b
Steve Brewer <svpersteve@users.noreply.github.com>
2018-12-02 12:32:03 +0100

    Start index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/** @format */
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => (
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('AwesomeProject', () => App);
