7cefc5fa81c8bb772d2d5cc14de2a5e2c212c41b
Steve Brewer <svpersteve@users.noreply.github.com>
2018-12-02 13:43:10 +0100

    Set state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,13 @@ import { View, Text } from 'react-native';
 import Axios from 'axios';
 
 class AlbumList extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      albums: []
+    };
+  }
+
   componentWillMount() {
     Axios
       .get('https://rallycoding.herokuapp.com/api/music_albums')
