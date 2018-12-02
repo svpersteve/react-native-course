1cf99534602c043f5fe6d4f1c0ef8acf4726846b
Steve Brewer <svpersteve@users.noreply.github.com>
2018-12-02 16:02:43 +0100

    Create card component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = (props) => {
   const album = props.album;
 
   return (
-    <Text>
-      {album.title}
-    </Text>
+    <View>
+      <Card>
+        <Text>{album.title}</Text>
+        <Text>{album.artist}</Text>
+      </Card>
+    </View>
   )
 };
 
