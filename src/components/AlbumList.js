9e4939196974e62ece933f5adebaae498cc69f47
Steve Brewer <svpersteve@users.noreply.github.com>
2018-12-02 13:40:03 +0100

    Add get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    Axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response))
+  }
+
   render() {
     return (
       <View>
